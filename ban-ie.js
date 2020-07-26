203852e3014d29ffc668024c378090ed9f46839f
Jap-Kor-Earthquake <kangdooho01@gmail.com>
2020-07-26 14:26:27 +0900

    Add files via upload

---
+++ ban-ie.js
@@ -0,0 +1,15 @@
+﻿// 익스플로러 체크
+
+var agent = navigator.userAgent.toLowerCase();
+
+if ( (navigator.appName == 'Netscape' && navigator.userAgent.search('Trident') != -1) || (agent.indexOf("msie") != -1) ) {
+
+  alert("익스플로러 브라우저는 보안상 접속하실수 없습니다.");
+  window.history.back()
+}
+
+else {
+
+  
+
+}
