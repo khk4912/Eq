ade4b5e347ba01da8cc4fd506bac28ff392e6bfe
Jap-Kor-Earthquake <kangdooho01@gmail.com>
2020-07-26 14:23:04 +0900

    Update 차단.js

--- 차단.js
+++ 차단.js
@@ -4,8 +4,8 @@ var agent = navigator.userAgent.toLowerCase();
 
 if ( (navigator.appName == 'Netscape' && navigator.userAgent.search('Trident') != -1) || (agent.indexOf("msie") != -1) ) {
 
-  alert("인터넷 익스플로러 브라우저 입니다.");
-
+  alert("익스플로러 브라우저는 보안상 접속하실수 없습니다.");
+  window.history.back()
 }
 
 else {
@@ -16,36 +16,3 @@ else {
 
 
 
-// 크롬 체크
-
-var agent = navigator.userAgent.toLowerCase();
-
-if (agent.indexOf("chrome") != -1) {
-
-  alert("크롬 브라우저입니다.");
-
-}
-
-
-
-// 사파리나 체크
-
-var agent = navigator.userAgent.toLowerCase();
-
-if (agent.indexOf("safari") != -1) {
-
-  alert("사파리 브라우저입니다.");
-
-}
-
-
-
-// 파이어폭스 체크
-
-var agent = navigator.userAgent.toLowerCase();
-
-if (agent.indexOf("firefox") != -1) {
-
-  alert("파이어폭스 브라우저입니다.");
-
-}
