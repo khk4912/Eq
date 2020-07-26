40c333e291c72a500617b11aac2c29901513422f
Jap-Kor-Earthquake <kangdooho01@gmail.com>
2020-07-26 14:34:25 +0900

    Add files via upload

---
+++ ip-ban.js
@@ -0,0 +1,12 @@
+﻿<script language="JavaScript">
+
+netscape = (navigator.appName.indexOf("Netscape") != -1);
+version4 = (navigator.appVersion.indexOf("4.") != -1);
+
+if (netscape && version4) {
+ip = "" + java.net.InetAddress.getLocalHost().getHostAddress();
+if (ip.indexOf("218.147.155.11") >= -1){
+alert("IP BAN ERROR CODE:001");
+history.go(-1);
+}
+}
