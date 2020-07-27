0c4b6eb7e3f8b8aacc809bceb67135cfd58efa04
Jap-Kor-Earthquake <kangdooho01@gmail.com>
2020-07-27 13:09:32 +0900

    Update ban-ie.js

--- ban-ie.js
+++ ban-ie.js
@@ -5,7 +5,7 @@ var agent = navigator.userAgent.toLowerCase();
 if ( (navigator.appName == 'Netscape' && navigator.userAgent.search('Trident') != -1) || (agent.indexOf("msie") != -1) ) {
 
   alert("익스플로러 브라우저는 보안상 접속하실수 없습니다.");
-  window.history.back()
+  <meta http-equiv='refresh' content='0;url=https://jap-kor-earthquake.github.io/Eq/IE-BAN.html'>
 }
 
 else {
