1a8ab02f58f17ba5f51f1e7026be0be5ec60899c
KDH 07 <kangdooho01@gmail.com>
2020-08-02 12:46:32 +0900

    Update API.js

--- API.js
+++ API.js
@@ -3,13 +3,13 @@
 
 var xhr = new XMLHttpRequest();
 var url = 'http://apis.data.go.kr/1360000/EqkInfoService/getEqkMsg'; /*URL*/
-var queryParams = '?' + encodeURIComponent('ServiceKey') + '='+'b2GRB41kH6MZnAd3KB7kISb2aGBOXUIzJCVnQwKuVwgr0HOnmXh7HnLpG%2B1bcWmC2nlyDzKQfAZx345pihi70Q%3D%3D'; /*Service Key*/
-queryParams += '&' + encodeURIComponent('ServiceKey') + '=' + encodeURIComponent('b2GRB41kH6MZnAd3KB7kISb2aGBOXUIzJCVnQwKuVwgr0HOnmXh7HnLpG%2B1bcWmC2nlyDzKQfAZx345pihi70Q%3D%3D'); /**/
+var queryParams = '?' + encodeURIComponent('ServiceKey') + '='+'b2GRB41kH6MZnAd3KB7k???????????????????????????'; /*Service Key*/
+queryParams += '&' + encodeURIComponent('ServiceKey') + '=' + encodeURIComponent('b2GRB41kH6MZnAd3KB7k???????????????????????????'); /**/
 queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /**/
 queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /**/
 queryParams += '&' + encodeURIComponent('dataType') + '=' + encodeURIComponent('XML'); /**/
-queryParams += '&' + encodeURIComponent('fromTmFc') + '=' + encodeURIComponent('20200801'); /**/
-queryParams += '&' + encodeURIComponent('toTmFc') + '=' + encodeURIComponent('20200802'); /**/
+queryParams += '&' + encodeURIComponent('fromTmFc') + '=' + encodeURIComponent('20171101'); /**/
+queryParams += '&' + encodeURIComponent('toTmFc') + '=' + encodeURIComponent('20171129'); /**/
 xhr.open('GET', url + queryParams);
 xhr.onreadystatechange = function () {
     if (this.readyState == 4) {
@@ -17,3 +17,4 @@ xhr.onreadystatechange = function () {
     }
 };
 
+xhr.send('');
