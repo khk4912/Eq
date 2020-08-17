bb6f3c2fc9c4eb8c85abec920c53f58fa79c83b2
KDH 07 <kangdooho01@gmail.com>
2020-08-18 00:04:03 +0900

    Add files via upload

---
+++ IE.js
@@ -0,0 +1,9 @@
+﻿// 익스플로러 체크
+
+var agent = navigator.userAgent.toLowerCase();
+
+if ( (navigator.appName == 'Netscape' && navigator.userAgent.search('Trident') != -1) || (agent.indexOf("msie") != -1) ) {
+
+  alert("본 웹 사이트는 익스플로러 브라우저 사용을 권장하지 않습니다.");
+
+}
